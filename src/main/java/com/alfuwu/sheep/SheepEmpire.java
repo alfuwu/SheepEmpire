7f6b89524cf201817fd4df860f210db80733658f
Alfred <eldritch.alfred@outlook.com>
2024-11-28 15:42:04 -0800

    skeleton project

---
+++ src/main/java/com/alfuwu/sheep/SheepEmpire.java
@@ -0,0 +1,17 @@
+package com.alfuwu.sheep;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SheepEmpire extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
